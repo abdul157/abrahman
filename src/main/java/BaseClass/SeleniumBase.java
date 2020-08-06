6e714e0e691d6485b2c241fb296d34074937eb4c
cicd <cicdcicd@gmail.com>
2020-08-06 23:01:12 +0530

    code change firefox

--- src/main/java/BaseClass/SeleniumBase.java
+++ src/main/java/BaseClass/SeleniumBase.java
@@ -1,19 +1,19 @@
 package BaseClass;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumBase {
     protected WebDriver driver;
-    public void startChromeBrowser(String url) {
-        System.setProperty("webdriver.chrome.driver", "D:\\driver\\chromedriver.exe");
+    public void startFirefoxBrowser(String url) {
+        System.setProperty("webdriver.gecko.driver", "/home/cicd/abd/projects/geckodriver-v0.27.0-linux64/geckodriver");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
-         driver = new ChromeDriver(options);
+         driver = new FirefoxDriver(options);
         driver.get(url);
     }
-    public void closeChromeBrowser()
+    public void closeFirefoxBrowser()
     {
         driver.close();
     }
--- src/test/java/Util/UtilClass.java
+++ src/test/java/Util/UtilClass.java
@@ -1,28 +1,21 @@
 package Util;
 
 import BaseClass.SeleniumBase;
-import org.apache.poi.xssf.usermodel.XSSFCell;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
-
-import java.io.IOException;
 
 public class UtilClass extends SeleniumBase {
 
     @BeforeMethod
     public void beforeHooks()
     {
-       startChromeBrowser("http://google.com");
+       startFirefoxBrowser("http://google.com");
     }
 
     @AfterMethod
     public void afterHooks()
     {
-      closeChromeBrowser();
+      closeFirefoxBrowser();
     }
 
 }
